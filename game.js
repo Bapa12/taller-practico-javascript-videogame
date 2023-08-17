597f79a44d673aeae4d704a58fd58141dc30a818
Bapa Bozinovich <bapa.bc04@gmail.com>
2023-08-17 17:58:59 -0400

    Added lives to the player and the restart of the game when the player loses them

--- game.js
+++ game.js
@@ -8,6 +8,7 @@ const btnDown = document.querySelector('#down');
 let canvasSize;
 let elementsSize;
 let level = 0;
+let lives = 3;
 
 const playerPosition = {
   x: undefined,
@@ -104,7 +105,7 @@ function movePlayer() {
   });
 
   if (enemyCollision) {
-    console.log('Chocaste contra un enemigo 😵!');
+    levelFailed();
   }
 
   game.fillText(emojis['PLAYER'], playerPosition.x, playerPosition.y);
@@ -113,6 +114,21 @@ function movePlayer() {
 function levelWon() {
   console.log('Subiste de nivel!');
   level++;
+  startGame();
+}
+
+function levelFailed() {
+  console.log('Chocaste contra un enemigo 😵!');
+  lives--;
+
+console.log(lives);
+
+  if (lives <= 0) {
+    level = 0;
+  }
+
+  playerPosition.x = undefined;
+  playerPosition.y = undefined;
   startGame();
 }
 
